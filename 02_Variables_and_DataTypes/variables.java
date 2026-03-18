5419487550c88be1e0769e552441859302e6c45d
Akhilesh Aher <aherakhilesh88@gmail.com>
2026-03-18 23:05:48 +0530

    Variables and Datatypes in Java

---
+++ 02_Variables_and_DataTypes/variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String [] args) {
+        byte age = 34;
+        int count = 56;
+        short num = 87;
+        long ageDino = 56666666L;
+        char ch = 'A';
+        float fl = 5.6f;
+        double dl = 4.666D;
+        boolean a = true;
+        String str = "Akhilesh";
+        System.out.println(age);
+    }
+}
